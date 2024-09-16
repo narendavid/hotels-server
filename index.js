620e28c19d2182045287c839f04604354ea1c39c
Naren Luna <narenkun96@gmail.com>
2024-09-16 13:37:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const PORT = process.env.PORT || 5000
+app.use(cors())
+
+app.get('/', (_, res) => {
+    res.send("Welcome to my API")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
